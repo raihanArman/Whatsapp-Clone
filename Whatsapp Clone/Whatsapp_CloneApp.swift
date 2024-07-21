24dee54032e3ebadc1c90af9315ee387bf59ae9b
raihanArman <raihanarman8@gmail.com>
2024-07-21 21:44:20 +0700

    Initial Commit

---
+++ Whatsapp Clone/Whatsapp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whatsapp_CloneApp.swift
+//  Whatsapp Clone
+//
+//  Created by Raihan Arman on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Whatsapp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
