0bc8eed8bccc47c363bae2a78cb4e389c728903e
raihanArman <raihanarman8@gmail.com>
2024-07-22 07:44:30 +0700

    setup tab view

---
+++ Whatsapp Clone/MainTabView.swift
@@ -0,0 +1,95 @@
+//
+//  MainTabView.swift
+//  Whatsapp Clone
+//
+//  Created by Raihan Arman on 22/07/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    init() {
+        makeTabBarOptions()
+    }
+    
+    var body: some View {
+        TabView {
+            placeHolderItemView("Updates")
+                .tabItem {
+                    Image(systemName: Tab.updates.icon)
+                    Text(Tab.updates.title)
+                }
+            placeHolderItemView("Calls")
+                .tabItem {
+                    Image(systemName: Tab.calls.icon)
+                    Text(Tab.calls.title)
+                }
+            placeHolderItemView("Communities")
+                .tabItem {
+                    Image(systemName: Tab.communities.icon)
+                    Text(Tab.communities.title)
+                }
+            placeHolderItemView("Chats")
+                .tabItem {
+                    Image(systemName: Tab.chats.icon)
+                    Text(Tab.chats.title)
+                }
+            placeHolderItemView("Settings")
+                .tabItem {
+                    Image(systemName: Tab.settings.icon)
+                    Text(Tab.settings.title)
+                }
+        }
+    }
+    
+    private func makeTabBarOptions() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+}
+
+extension MainTabView {
+    private func placeHolderItemView(_ title: String) -> some View {
+        ScrollView {
+            VStack {
+                ForEach(0..<120) { _ in
+                    Text(title)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 120)
+                        .background(Color.green.opacity(0.5))
+                }
+            }
+        }
+    }
+    
+    private enum Tab: String {
+        case updates, calls, communities, chats, settings
+        
+        fileprivate var title: String {
+            return rawValue.capitalized
+        }
+        
+        fileprivate var icon: String {
+            switch self {
+            case .updates:
+                return "circle.dashed.inset.filled"
+            case .calls:
+                return "phone"
+            case .communities:
+                return "person.3"
+            case .chats:
+                return "message"
+            case .settings:
+                return "gear"
+            }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
